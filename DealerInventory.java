7569a13d7d77ca0642375ba8ec6077254f3c4bf8
Brian Gunther <brian.gunther@csuglobal.edu>
2024-04-04 22:26:35 -0700

    finished removeAutomobile() method

--- DealerInventory.java
+++ DealerInventory.java
@@ -6,20 +6,19 @@ public class DealerInventory {
    // Define a new ArrayList that will contain inventory objects of the Automobile class
    private ArrayList<Automobile> dealerInventory = new ArrayList<Automobile>();
    
-   public boolean checkInventory(String vin) {
+   public int checkInventory(String vin) {
       for (int i = 0; i < dealerInventory.size(); i++) {
          if (vin.compareTo(dealerInventory.get(i).getVin()) == 0) {
-            return true;
+            return i;
          }
       }
-      return false;
+      return -1;
    }
-   /**
-    * Method to create a new automobile in the dealer inventory ArrayList
-    */
+
+   // Method to add an automobile object to inventory
    public Automobile addAutomobile(String vin, String make, String model, String color, int year, int mileage, BigDecimal price) {
       try {
-         if (!checkInventory(vin)) {
+         if (checkInventory(vin) < 0) {
             Automobile newAuto = new Automobile(vin, make, model, color, year, mileage, price);
             dealerInventory.add(newAuto);
             return newAuto;
@@ -34,6 +33,27 @@ public class DealerInventory {
       }
 
       return null;
+   }
+   
+   // Method to remove an automobile from inventory (based on VIN()
+   public boolean removeAutomobile(String vin) {
+      try {
+         int vinIndex = checkInventory(vin);
+         
+         if (vinIndex >= 0) {
+            dealerInventory.remove(vinIndex);
+            return true;
+         }
+         else {
+            System.out.println("Automobile could not be found in inventory");
+         }
+      }
+      catch (Exception e) {
+         System.out.println(e.getMessage());
+         System.out.println("ERROR: Could not remove automobile.");         
+      }
+      
+      return false;
    }
 
    public static void main(String[] args) {
